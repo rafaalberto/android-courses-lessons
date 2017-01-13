4d03ae55d35f053b8f289e427fc8484793f3194e
Rafael Alberto <rafael.alberto1703@gmail.com>
2017-01-12 22:08:18 -0200

    implemento lista no projeto Miwok

--- Miwok/app/src/main/java/ra/com/br/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/ra/com/br/miwok/WordAdapter.java
@@ -1,9 +1,11 @@
 package ra.com.br.miwok;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Word currentyWord = getItem(position);
+        TextView textViewMiwok = (TextView) listItemView.findViewById(R.id.text_view_miwok);
+        textViewMiwok.setText(currentyWord.getMiwokTranslation());
+
+        TextView textViewDefault = (TextView) listItemView.findViewById(R.id.text_view_default);
+        textViewDefault.setText(currentyWord.getDefaultTranslation());
+
+        return listItemView;
     }
 }
