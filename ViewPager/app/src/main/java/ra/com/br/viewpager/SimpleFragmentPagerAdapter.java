af810ba7a5d0632402566384f2a681b907138349
Rafael Alberto <rafael.alberto1703@gmail.com>
2017-03-12 15:07:18 -0300

    refatorando projeto ViewPager

--- ViewPager/app/src/main/java/ra/com/br/viewpager/SimpleFragmentPagerAdapter.java
+++ ViewPager/app/src/main/java/ra/com/br/viewpager/SimpleFragmentPagerAdapter.java
@@ -6,7 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    private static final int NUMBER_OF_PAGES = 5;
+    private static final int NUMBER_OF_PAGES = 7;
 
     SimpleFragmentPagerAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
@@ -16,19 +16,25 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         switch (position) {
             case 0: {
-                return new MondayFragment();
+                return new WeekFragment().newInstance(R.string.sunday);
             }
             case 1: {
-                return new TuesdayFragment();
+                return new WeekFragment().newInstance(R.string.monday);
             }
             case 2: {
-                return new WednesdayFragment();
+                return new WeekFragment().newInstance(R.string.tuesday);
             }
             case 3: {
-                return new ThursdayFragment();
+                return new WeekFragment().newInstance(R.string.wednesday);
             }
             case 4: {
-                return new FridayFragment();
+                return new WeekFragment().newInstance(R.string.thursday);
+            }
+            case 5: {
+                return new WeekFragment().newInstance(R.string.friday);
+            }
+            case 6: {
+                return new WeekFragment().newInstance(R.string.saturday);
             }
         }
         return null;
