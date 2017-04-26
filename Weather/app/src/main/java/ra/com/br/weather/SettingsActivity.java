3124c7e51e6c898e2c4c0df85864a32f9aad1ceb
Rafael Alberto <rafael.alberto@supportweb.com.br>
2017-04-26 13:36:10 -0300

    adicionando preferências Weather

--- Weather/app/src/main/java/ra/com/br/weather/SettingsActivity.java
+++ Weather/app/src/main/java/ra/com/br/weather/SettingsActivity.java
@@ -1,6 +1,11 @@
 package ra.com.br.weather;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,5 +18,29 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public static class WeatherPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference location = findPreference(getString(R.string.settings_location_key));
+            bindPreferenceSummaryToValue(location);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            preference.setSummary(newValue.toString());
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference){
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            onPreferenceChange(preference, sharedPreferences.getString(preference.getKey(), ""));
+        }
     }
 }
