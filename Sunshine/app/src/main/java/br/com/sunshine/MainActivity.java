1c8907e91ba936d901d594fba659b52d4981b705
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-03-05 18:36:37 -0300

    atualização

--- Sunshine/app/src/main/java/br/com/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/br/com/sunshine/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.sunshine;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.net.URL;
@@ -16,6 +20,8 @@ import br.com.sunshine.utilities.OpenWeatherJsonUtils;
 public class MainActivity extends AppCompatActivity {
 
     TextView textViewWeatherData;
+    TextView textViewErrorMessage;
+    ProgressBar progressBarLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +29,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         textViewWeatherData = (TextView) findViewById(R.id.text_view_weather_data);
+        textViewErrorMessage = (TextView) findViewById(R.id.text_view_error_message);
+        progressBarLoading = (ProgressBar) findViewById(R.id.progressbar_loading);
         loadWeatherData();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.action_menu_refresh) {
+            textViewWeatherData.setText("");
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBarLoading.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected String[] doInBackground(String... params) {
-            if(params.length == 0) {
+            if (params.length == 0) {
                 return null;
             }
 
@@ -50,16 +86,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
-            if(weatherData != null) {
-                for(String weatherString : weatherData) {
+            progressBarLoading.setVisibility(View.INVISIBLE);
+            if (weatherData != null) {
+                for (String weatherString : weatherData) {
                     textViewWeatherData.append((weatherString) + "\n\n");
                 }
+            } else {
+                textViewWeatherData.setVisibility(View.INVISIBLE);
+                textViewErrorMessage.setVisibility(View.VISIBLE);
             }
         }
-    }
-
-    private void loadWeatherData() {
-        String location = SunshinePreferences.getPreferredWeatherLocation(this);
-        new FetchWeatherTask().execute(location);
     }
 }
