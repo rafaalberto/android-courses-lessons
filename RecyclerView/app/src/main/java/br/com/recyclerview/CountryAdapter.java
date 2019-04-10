1f773f8cff3478c5515b0cabc0314e79a6588c35
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-04-09 21:00:56 -0300

    add simple recyclerview

---
+++ RecyclerView/app/src/main/java/br/com/recyclerview/CountryAdapter.java
@@ -0,0 +1,55 @@
+package br.com.recyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryAdapterViewHolder> {
+
+    private List<String> countries;
+
+    @NonNull
+    @Override
+    public CountryAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int layoutIdListItem = R.layout.country_list_item;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View view = layoutInflater.inflate(layoutIdListItem, viewGroup, false);
+        return new CountryAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryAdapterViewHolder countryAdapterViewHolder, int i) {
+        String city = this.countries.get(i);
+        countryAdapterViewHolder.textViewCityItem.setText(city);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(this.countries.size() == 0) return 0;
+        return this.countries.size();
+    }
+
+    public void setCities(List<String> countries) {
+        this.countries = countries;
+        notifyDataSetChanged();
+    }
+
+    public class CountryAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textViewCityItem;
+
+        public CountryAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewCityItem = itemView.findViewById(R.id.text_view_city_item);
+        }
+    }
+
+}
---
+++ RecyclerView/app/src/main/java/br/com/recyclerview/MainActivity.java
@@ -0,0 +1,54 @@
+package br.com.recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CountryAdapter countryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview_list);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        countryAdapter = new CountryAdapter();
+        recyclerView.setAdapter(countryAdapter);
+
+        List<String> countries = new ArrayList<>();
+        countries.add("Brazil");
+        countries.add("United States");
+        countries.add("Germany");
+        countries.add("Portugal");
+        countries.add("Canada");
+        countries.add("Chile");
+        countries.add("France");
+        countries.add("England");
+        countries.add("China");
+        countries.add("Japan");
+        countries.add("Colombia");
+        countries.add("Italy");
+        countries.add("Spain");
+        countries.add("Mexico");
+        countries.add("Russia");
+        countries.add("South Africa");
+        countries.add("Australia");
+
+        Collections.sort(countries, (item1, item2) -> item1.compareTo(item2));
+
+        countryAdapter.setCities(countries);
+    }
+}
