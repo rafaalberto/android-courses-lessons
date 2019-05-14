f06befcd8ba8cb851e74daf74f2c0e8fe540b31e
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-05-14 20:48:15 -0300

    implementation of viewmodel and adapter

--- AndroidArchitecture/app/src/main/java/br/com/architecture/MainActivity.java
+++ AndroidArchitecture/app/src/main/java/br/com/architecture/MainActivity.java
@@ -1,13 +1,38 @@
 package br.com.architecture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(noteAdapter);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.findAll().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                noteAdapter.setNotes(notes);
+            }
+        });
+
     }
 }
---
+++ AndroidArchitecture/app/src/main/java/br/com/architecture/NoteAdapter.java
@@ -0,0 +1,55 @@
+package br.com.architecture;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.note_item, viewGroup, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int position) {
+        Note note = notes.get(position);
+        noteHolder.textViewTitle.setText(note.getTitle());
+        noteHolder.textViewDescription.setText(note.getDescription());
+        noteHolder.textViewPriority.setText(String.valueOf(note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        TextView textViewTitle;
+        TextView textViewDescription;
+        TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
