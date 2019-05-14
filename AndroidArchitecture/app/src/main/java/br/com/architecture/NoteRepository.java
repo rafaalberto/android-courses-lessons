cba241699bd686752168108f64245b937c9766c3
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-05-13 22:53:08 -0300

    inserindo projeto architecture padrão MVVM

---
+++ AndroidArchitecture/app/src/main/java/br/com/architecture/NoteRepository.java
@@ -0,0 +1,99 @@
+package br.com.architecture;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao();
+        allNotes = noteDao.findAll();
+    }
+
+    public void insert(Note note) {
+        new InsertNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void update(Note note) {
+        new UpdateNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void delete(Note note) {
+        new DeleteNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void deleteAll() {
+        new DeleteAllAsyncTask(noteDao).execute();
+    }
+
+    public LiveData<List<Note>> findAll() {
+        return allNotes;
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private InsertNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private UpdateNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private DeleteNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private DeleteAllAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAll();
+            return null;
+        }
+    }
+}
